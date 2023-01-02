a82fe1af2036ab7db48b245479606d1a4765e2da
Restyled.io <commits@restyled.io>
2023-01-02 15:25:10 +0000

    Restyled by clang-format

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -13,7 +13,7 @@ const {
   updateUserRole,
   deleteUser,
 } = require('../controllers/userController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
+const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth');
 const router = express.Router();
 const upload = require('../utils/multer');
 
@@ -26,14 +26,12 @@ router.route('/me').get(isAuthenticatedUser, getUserDetails);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
-router
-  .route('/admin/users')
-  .get(isAuthenticatedUser, authorizeRoles('admin'), getAllUser);
+router.route('/admin/users')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getAllUser);
 
-router
-  .route('/admin/user/:id')
-  .get(isAuthenticatedUser, authorizeRoles('admin'), getSingleUser)
-  .put(isAuthenticatedUser, authorizeRoles('admin'), updateUserRole)
-  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
+router.route('/admin/user/:id')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getSingleUser)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateUserRole)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
