08ce1eb6f52e3c97c2096327f4e9e17dd837135c
Restyled.io <commits@restyled.io>
2023-01-10 04:15:01 +0000

    Restyled by prettier

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,23 +1,23 @@
-const nodeMailer = require('nodemailer');
+const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
-    host : process.env.SMTP_HOST,
+    host: process.env.SMTP_HOST,
     // service: process.env.SMTP_SERVICE,
-    port : process.env.SMTP_PORT,
+    port: process.env.SMTP_PORT,
     // secure: true, // true for 465, false for other ports
     // secureConnection: false,
-    auth : {
-      user : process.env.SMTP_USER,
-      pass : process.env.SMTP_PASS,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
   const mailOptions = {
-    from : process.env.SMTP_MAIL,
-    to : options.email,
-    subject : options.subject,
-    text : options.message,
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
   };
 
   return await transporter.sendMail(mailOptions);
