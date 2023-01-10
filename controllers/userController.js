0092e2f9ef0b3a3a1774bfb858474ef9f7356806
Restyled.io <commits@restyled.io>
2023-01-10 04:15:00 +0000

    Restyled by clang-format

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,18 +9,18 @@ const cloudinary = require('../utils/cloudinaryConfig');
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
   const result = await cloudinary.uploader.upload(req.file.path, {
-    folder: 'avatars',
+    folder : 'avatars',
   });
 
-  const { name, email, password } = req.body;
+  const {name, email, password} = req.body;
 
   const user = await User.create({
     name,
     email,
     password,
-    avatar: {
-      public_id: result.public_id,
-      url: result.secure_url,
+    avatar : {
+      public_id : result.public_id,
+      url : result.secure_url,
     },
   });
 
@@ -29,7 +29,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
 // Login User
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body;
 
   // checking if user has given password and email both
 
@@ -37,7 +37,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHander('Please Enter Email & Password', 400));
   }
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({email}).select('+password');
 
   if (!user) {
     return next(new ErrorHander('Invalid email', 401));
@@ -55,13 +55,13 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 // Logout User
 exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.cookie('token', null, {
-    expires: new Date(Date.now()),
-    httpOnly: true,
+    expires : new Date(Date.now()),
+    httpOnly : true,
   });
 
   res.status(200).json({
-    success: true,
-    message: 'Logged Out',
+    success : true,
+    message : 'Logged Out',
   });
 });
 
@@ -70,7 +70,7 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findById(req.user.id);
 
   res.status(200).json({
-    success: true,
+    success : true,
     user,
   });
 });
@@ -80,7 +80,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   if (!req.body.email) {
     return next(new ErrorHander('Email is required', 400));
   }
-  const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({email : req.body.email});
 
   if (!user) {
     return next(new ErrorHander('User not found', 404));
@@ -89,31 +89,32 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   // Get ResetPassword Token
   const resetToken = user.getResetPasswordToken();
 
-  await user.save({ validateBeforeSave: false });
+  await user.save({validateBeforeSave : false});
 
   // const resetPasswordUrl = `${req.protocol}://${req.get(
   //   'host'
   // )}/api/v1/password/reset/${resetToken}`;
   const resetPasswordUrl = `http://localhost:3000/password/reset/${resetToken}`;
 
-  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+  const message = `Your password reset token is :- \n\n ${
+      resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
 
   try {
     await sendEmail({
-      email: user.email,
-      subject: `Ecommerce Password Recovery`,
+      email : user.email,
+      subject : `Ecommerce Password Recovery`,
       message,
     });
 
     res.status(200).json({
-      success: true,
-      message: `Email sent to ${user.email} successfully`,
+      success : true,
+      message : `Email sent to ${user.email} successfully`,
     });
   } catch (error) {
     user.resetPasswordToken = undefined;
     user.resetPasswordExpire = undefined;
 
-    await user.save({ validateBeforeSave: false });
+    await user.save({validateBeforeSave : false});
 
     return next(new ErrorHander(error.message, 500));
   }
@@ -122,23 +123,17 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 // Reset Password
 exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   // creating token hash
-  const resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(req.params.token)
-    .digest('hex');
+  const resetPasswordToken =
+      crypto.createHash('sha256').update(req.params.token).digest('hex');
 
   const user = await User.findOne({
     resetPasswordToken,
-    resetPasswordExpire: { $gt: Date.now() },
+    resetPasswordExpire : {$gt : Date.now()},
   });
 
   if (!user) {
-    return next(
-      new ErrorHander(
-        'Reset Password Token is invalid or has been expired',
-        400
-      )
-    );
+    return next(new ErrorHander(
+        'Reset Password Token is invalid or has been expired', 400));
   }
 
   if (req.body.password !== req.body.confirmPassword) {
@@ -159,7 +154,7 @@ exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findById(req.user.id);
 
   res.status(200).json({
-    success: true,
+    success : true,
     user,
   });
 });
@@ -188,8 +183,8 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
 // update User Profile
 exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
   const newUserData = {
-    name: req.body.name,
-    email: req.body.email,
+    name : req.body.name,
+    email : req.body.email,
   };
 
   // if (req.body.avatar !== "") {
@@ -212,13 +207,13 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
   // }
 
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
+    new : true,
+    runValidators : true,
+    useFindAndModify : false,
   });
 
   res.status(200).json({
-    success: true,
+    success : true,
     user,
   });
 });
@@ -228,7 +223,7 @@ exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
   const users = await User.find();
 
   res.status(200).json({
-    success: true,
+    success : true,
     users,
   });
 });
@@ -239,12 +234,11 @@ exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
 
   if (!user) {
     return next(
-      new ErrorHander(`User does not exist with Id: ${req.params.id}`)
-    );
+        new ErrorHander(`User does not exist with Id: ${req.params.id}`));
   }
 
   res.status(200).json({
-    success: true,
+    success : true,
     user,
   });
 });
@@ -252,19 +246,19 @@ exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
 // update User Role -- Admin
 exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
   const newUserData = {
-    name: req.body.name,
-    email: req.body.email,
-    role: req.body.role,
+    name : req.body.name,
+    email : req.body.email,
+    role : req.body.role,
   };
 
   await User.findByIdAndUpdate(req.params.id, newUserData, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
+    new : true,
+    runValidators : true,
+    useFindAndModify : false,
   });
 
   res.status(200).json({
-    success: true,
+    success : true,
   });
 });
 
@@ -274,14 +268,13 @@ exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
 
   if (!user) {
     return next(
-      new ErrorHander(`User does not exist with Id: ${req.params.id}`, 400)
-    );
+        new ErrorHander(`User does not exist with Id: ${req.params.id}`, 400));
   }
 
   await user.remove();
 
   res.status(200).json({
-    success: true,
-    message: 'User Deleted Successfully',
+    success : true,
+    message : 'User Deleted Successfully',
   });
 });
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,22 +2,22 @@ const nodeMailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    //service: process.env.SMTP_SERVICE,
-    port: process.env.SMTP_PORT,
+    host : process.env.SMTP_HOST,
+    // service: process.env.SMTP_SERVICE,
+    port : process.env.SMTP_PORT,
     // secure: true, // true for 465, false for other ports
     // secureConnection: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+    auth : {
+      user : process.env.SMTP_USER,
+      pass : process.env.SMTP_PASS,
     },
   });
 
   const mailOptions = {
-    from: process.env.SMTP_MAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
+    from : process.env.SMTP_MAIL,
+    to : options.email,
+    subject : options.subject,
+    text : options.message,
   };
 
   return await transporter.sendMail(mailOptions);
