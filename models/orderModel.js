b62a49776c9f3be3b0dcc359b0b8985e7140acd7
Restyled.io <commits@restyled.io>
2023-01-13 08:37:13 +0000

    Restyled by prettier

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,116 +1,116 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  shippingInfo : {
-    address : {
-      type : String,
-      required : true,
+  shippingInfo: {
+    address: {
+      type: String,
+      required: true,
     },
-    city : {
-      type : String,
-      required : true,
-    },
-
-    state : {
-      type : String,
-      required : true,
+    city: {
+      type: String,
+      required: true,
     },
 
-    country : {
-      type : String,
-      required : true,
+    state: {
+      type: String,
+      required: true,
     },
-    pinCode : {
-      type : Number,
-      required : true,
+
+    country: {
+      type: String,
+      required: true,
     },
-    phoneNo : {
-      type : Number,
-      required : true,
+    pinCode: {
+      type: Number,
+      required: true,
+    },
+    phoneNo: {
+      type: Number,
+      required: true,
     },
   },
-  orderItems : [
+  orderItems: [
     {
-      name : {
-        type : String,
-        required : true,
+      name: {
+        type: String,
+        required: true,
       },
-      price : {
-        type : Number,
-        required : true,
+      price: {
+        type: Number,
+        required: true,
       },
-      quantity : {
-        type : Number,
-        required : true,
+      quantity: {
+        type: Number,
+        required: true,
       },
-      image : {
-        type : String,
-        required : true,
+      image: {
+        type: String,
+        required: true,
       },
-      product : {
-        type : mongoose.Schema.ObjectId,
-        ref : 'Product',
-        required : true,
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+        required: true,
       },
     },
   ],
-  user : {
-    type : mongoose.Schema.ObjectId,
-    ref : 'User',
-    required : true,
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
   },
-  paymentInfo : {
-    id : {
-      type : String,
-      required : false,
+  paymentInfo: {
+    id: {
+      type: String,
+      required: false,
     },
-    status : {
-      type : String,
-      required : false,
+    status: {
+      type: String,
+      required: false,
     },
-    paymentStatus : {
-      type : String,
-      required : true,
-      default : 'Pending',
+    paymentStatus: {
+      type: String,
+      required: true,
+      default: "Pending",
     },
   },
-  paidAt : {
-    type : Date,
-    required : false,
+  paidAt: {
+    type: Date,
+    required: false,
   },
-  itemsPrice : {
-    type : Number,
-    required : true,
-    default : 0,
+  itemsPrice: {
+    type: Number,
+    required: true,
+    default: 0,
   },
-  taxPrice : {
-    type : Number,
-    required : true,
-    default : 0,
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0,
   },
-  shippingPrice : {
-    type : Number,
-    required : true,
-    default : 0,
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0,
   },
-  totalPrice : {
-    type : Number,
-    required : true,
-    default : 0,
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0,
   },
-  orderStatus : {
-    type : String,
-    required : true,
-    default : 'Processing',
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "Processing",
   },
-  deliveredAt : {
-    type : Date,
-    required : false,
+  deliveredAt: {
+    type: Date,
+    required: false,
   },
-  createdAt : {
-    type : Date,
-    default : Date.now,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
-module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
