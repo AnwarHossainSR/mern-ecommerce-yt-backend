7d650a4cf5606f81dc26160a9ca116d4d4b807fd
Restyled.io <commits@restyled.io>
2023-01-16 14:38:52 +0000

    Restyled by prettier

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,6 @@
-const User = require('../models/userModel');
-const Product = require('../models/productModel');
-const Order = require('../models/orderModel');
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const Order = require("../models/orderModel");
 
 // get Dashboard STate And Graph Data
 
@@ -24,28 +24,28 @@ exports.getDashboardStateAndGraphData = async (req, res) => {
     month.setMonth(month.getMonth() - i);
     const total = await Order.aggregate([
       {
-        $match : {
-          createdAt : {
-            $gte : new Date(month.setMonth(month.getMonth() - 1)),
-            $lte : new Date(month.setMonth(month.getMonth() + 1)),
+        $match: {
+          createdAt: {
+            $gte: new Date(month.setMonth(month.getMonth() - 1)),
+            $lte: new Date(month.setMonth(month.getMonth() + 1)),
           },
         },
       },
       {
-        $project : {
-          month : {$month : '$createdAt'},
-          totalPrice : '$totalPrice',
+        $project: {
+          month: { $month: "$createdAt" },
+          totalPrice: "$totalPrice",
         },
       },
       {
-        $group : {
-          _id : '$month',
-          total : {$sum : '$totalPrice'},
+        $group: {
+          _id: "$month",
+          total: { $sum: "$totalPrice" },
         },
       },
     ]);
 
-    months.push(month.toLocaleString('default', {month : 'short'}));
+    months.push(month.toLocaleString("default", { month: "short" }));
     revenue.push(total.length > 0 ? total[0].total : 0);
   }
 
@@ -54,16 +54,16 @@ exports.getDashboardStateAndGraphData = async (req, res) => {
 
   // return latest 5 orders with usr name, order date and total price
   const latestOrders = await Order.find()
-                           .sort({createdAt : -1})
-                           .limit(5)
-                           .populate('user', 'name')
-                           .select('user createdAt totalPrice');
+    .sort({ createdAt: -1 })
+    .limit(5)
+    .populate("user", "name")
+    .select("user createdAt totalPrice");
 
   let graphData = [];
   months.map((month, index) => {
     graphData.push({
-      name : month,
-      amount : revenue[index],
+      name: month,
+      amount: revenue[index],
     });
   });
 
