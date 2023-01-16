27ab79c6fd3a77fa22d9f33b82f3b20225f401eb
Restyled.io <commits@restyled.io>
2023-01-16 08:55:53 +0000

    Restyled by clang-format

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,14 +2,14 @@ const User = require('../models/userModel');
 const Product = require('../models/productModel');
 const Order = require('../models/orderModel');
 
-//get Dashboard STate And Graph Data
+// get Dashboard STate And Graph Data
 
 exports.getDashboardStateAndGraphData = async (req, res) => {
   const today = new Date();
   const lastMonth = new Date(today.setMonth(today.getMonth() - 1));
 
   const orders = await Order.find({
-    createdAt: { $gte: lastMonth },
+    createdAt : {$gte : lastMonth},
   });
 
   const totalOrders = orders.length;
@@ -20,7 +20,7 @@ exports.getDashboardStateAndGraphData = async (req, res) => {
 
   const totalUsers = await User.countDocuments();
 
-  //get last 12 months revenue with graph data
+  // get last 12 months revenue with graph data
   const months = [];
   const revenue = [];
 
@@ -29,46 +29,46 @@ exports.getDashboardStateAndGraphData = async (req, res) => {
     month.setMonth(month.getMonth() - i);
     const total = await Order.aggregate([
       {
-        $match: {
-          createdAt: {
-            $gte: new Date(month.setMonth(month.getMonth() - 1)),
-            $lte: new Date(month.setMonth(month.getMonth() + 1)),
+        $match : {
+          createdAt : {
+            $gte : new Date(month.setMonth(month.getMonth() - 1)),
+            $lte : new Date(month.setMonth(month.getMonth() + 1)),
           },
         },
       },
       {
-        $project: {
-          month: { $month: '$createdAt' },
-          totalPrice: '$totalPrice',
+        $project : {
+          month : {$month : '$createdAt'},
+          totalPrice : '$totalPrice',
         },
       },
       {
-        $group: {
-          _id: '$month',
-          total: { $sum: '$totalPrice' },
+        $group : {
+          _id : '$month',
+          total : {$sum : '$totalPrice'},
         },
       },
     ]);
 
-    months.push(month.toLocaleString('default', { month: 'short' }));
+    months.push(month.toLocaleString('default', {month : 'short'}));
     revenue.push(total.length > 0 ? total[0].total : 0);
   }
 
   months.reverse();
   revenue.reverse();
 
-  //return latest 5 orders with usr name, order date and total price
+  // return latest 5 orders with usr name, order date and total price
   const latestOrders = await Order.find()
-    .sort({ createdAt: -1 })
-    .limit(5)
-    .populate('user', 'name')
-    .select('user createdAt totalPrice');
+                           .sort({createdAt : -1})
+                           .limit(5)
+                           .populate('user', 'name')
+                           .select('user createdAt totalPrice');
 
   let graphData = [];
   months.map((month, index) => {
     graphData.push({
-      name: month,
-      amount: revenue[index],
+      name : month,
+      amount : revenue[index],
     });
   });
 
