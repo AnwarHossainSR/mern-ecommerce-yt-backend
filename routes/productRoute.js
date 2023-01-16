b81a4236f73d45d882bedc7a34a5ffbe9cefa7f8
Restyled.io <commits@restyled.io>
2023-01-16 06:07:36 +0000

    Restyled by clang-format

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -13,42 +13,30 @@ const {
   getProductReviews,
   deleteReview,
 } = require('../controllers/productController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
+const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth');
 const upload = require('../utils/multer');
 
 const router = express.Router();
 
 router.route('/products').get(getAllProducts);
-router
-  .route('/admin/products')
-  .get(isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
-router
-  .route('/admin/product/new')
-  .post(
-    isAuthenticatedUser,
-    authorizeRoles('admin'),
-    upload.single('image'),
-    createProduct
-  );
-router
-  .route('/admin/product/:id')
-  .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
-  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+router.route('/admin/products')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
+router.route('/admin/product/new')
+    .post(isAuthenticatedUser, authorizeRoles('admin'), upload.single('image'),
+          createProduct);
+router.route('/admin/product/:id')
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
-router
-  .route('/admin/dashboard')
-  .get(
-    isAuthenticatedUser,
-    authorizeRoles('admin'),
-    getDashboardStateAndGraphData
-  );
+router.route('/admin/dashboard')
+    .get(isAuthenticatedUser, authorizeRoles('admin'),
+         getDashboardStateAndGraphData);
 
 router.route('/product/:id').get(getProductDetails);
 router.route('/review').put(isAuthenticatedUser, createProductReview);
 
-router
-  .route('/reviews')
-  .get(getProductReviews)
-  .delete(isAuthenticatedUser, deleteReview);
+router.route('/reviews')
+    .get(getProductReviews)
+    .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
